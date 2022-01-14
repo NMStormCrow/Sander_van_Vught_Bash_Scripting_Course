cda7bb58f9abcef0fe40db8e6df343cea7145207
Thomas Crow <paladin677@gmail.com>
2022-01-14 16:16:02 -0700

    Initial commit

---
+++ exercise1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a script that copies the contents of the log file /var/log/messages to /var/log/messages.old and deletes the contents
+# of the /var/log/messages file
+
+cp /var/log/messages /var/log/messages
+cat /dev/null > /var/log/messages
+echo log file copied and cleaned up
+
+exit 0
