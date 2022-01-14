d5e7d3714bebef0db834757123ef42b883b489b8
Thomas Crow <paladin677@gmail.com>
2022-01-14 16:24:56 -0700

    Updated exercise1.sh

--- exercise1.sh
+++ exercise1.sh
@@ -2,6 +2,7 @@
 
 # Create a script that copies the contents of the log file /var/log/messages to /var/log/messages.old and deletes the contents
 # of the /var/log/messages file
+# usage ./exercise1.sh
 
 cp /var/log/messages /var/log/messages
 cat /dev/null > /var/log/messages
